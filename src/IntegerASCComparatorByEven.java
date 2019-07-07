6467649196a29446e0cdafde1e374c29455f679d
TexHaPb85 <16fi.m.yevtushenko@std.npu.edu.ua>
2019-07-07 12:34:16 +0300

    first task done

---
+++ src/IntegerASCComparatorByEven.java
@@ -0,0 +1,14 @@
+import java.util.Comparator;
+
+public class IntegerASCComparatorByEven implements Comparator<Integer>{
+    @Override
+    public int compare(Integer o1, Integer o2) {
+        if (Math.abs(o1 % 2) == Math.abs(o2 % 2)) {
+            return o1 - o2;
+        } else if (o1 % 2 == 0) {
+            return -1;
+        } else if(o2 % 2 ==0){
+            return 1;
+        }else return 0;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Set<Integer> numbers = new TreeSet<>(new IntegerASCComparatorByEven());
+        numbers.add(2);
+        numbers.add(1);
+        numbers.add(4);
+        numbers.add(-4);
+        numbers.add(-1);
+        numbers.add(-5);
+        numbers.add(-2);
+        numbers.add(3);
+        numbers.add(5);
+        numbers.add(7);
+        numbers.add(6);
+
+        System.out.println(numbers);
+    }
+}
