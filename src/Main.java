137e81f20d8835679d2036b78f35b6d8721d0318
TexHaPb85 <16fi.m.yevtushenko@std.npu.edu.ua>
2019-07-07 14:04:49 +0300

    static blocks added

--- src/Main.java
+++ src/Main.java
@@ -3,20 +3,34 @@ import java.util.TreeSet;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Set<Integer> numbers = new TreeSet<>(new IntegerASCComparatorByEven());
-        numbers.add(2);
-        numbers.add(1);
-        numbers.add(4);
-        numbers.add(-4);
-        numbers.add(-1);
-        numbers.add(-5);
-        numbers.add(-2);
-        numbers.add(3);
-        numbers.add(5);
-        numbers.add(7);
-        numbers.add(6);
+    private static Set<Integer> numbers = new TreeSet<>(new IntegerASCComparatorByEven());
 
+    static {
+        numbers.add(2);
         System.out.println(numbers);
+        numbers.add(1);
+        System.out.println(numbers);
+        numbers.add(4);
+        System.out.println(numbers);
+        numbers.add(-4);
+        System.out.println(numbers);
+        numbers.add(-1);
+        System.out.println(numbers);
+        numbers.add(-5);
+        System.out.println(numbers);
+        numbers.add(-2);
+        System.out.println(numbers);
+        numbers.add(3);
+        System.out.println(numbers);
+        numbers.add(5);
+        System.out.println(numbers);
+        numbers.add(7);
+        System.out.println(numbers);
+        numbers.add(6);
+        System.out.println(numbers);
+    }
+
+    public static void main(String[] args) {
+
     }
 }
